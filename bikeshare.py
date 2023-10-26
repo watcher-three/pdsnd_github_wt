364a883c833511ff5a95a06a7b20d91db895173d
Justin Becker <e383529@us.lmco.com>
2023-10-25 23:10:37 -0400

    Modified wording, line 254.

--- bikeshare.py
+++ bikeshare.py
@@ -251,7 +251,7 @@ def main():
         trip_duration_stats(df)
         user_stats(df)
 
-        restart = input("\nWould you like to restart? Enter yes or no.\n")
+        restart = input("\nWould you like to restart the program? Enter yes or no.\n")
         if restart.lower() != 'yes':
             break
 
